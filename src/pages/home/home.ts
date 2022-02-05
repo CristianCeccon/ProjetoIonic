b2f9c4097ed4049abc944860baeedc8f9c166cda
CristianCeccon <cristian_ceccon45@hotmail.com>
2022-02-05 14:26:31 -0300

    Listando produtos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public produtos;
+
   constructor(public navCtrl: NavController) {
+
+    this.produtos= [
+      {nome:'Arroz', preco: 34},
+      {nome:'Feijão', preco: 28},
+      {nome:'Macarrão', preco: 80},
+      {nome:'Panetone', preco: 40},
+      {nome:'Leite Caixa', preco: 7},
+      {nome:'Batata', preco: 10},
+      {nome:'Banana', preco: 5},
+      {nome:'Carne Alcatra', preco: 200},
+      {nome:'Peixe Sardinha', preco: 100},
+      {nome:'Suco de caixa', preco: 20},
+    ]
 
   }
 
